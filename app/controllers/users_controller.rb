af4dcf494379f89f4ea13c41bc60ae85cc16a289
kumini88 <gkumini8874424362@yahoo.co.jp>
2017-11-02 03:19:53 +0000

    rakuten api

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:show]
+  
   def show
     @user = User.find_by(params[:id])
   end
@@ -22,5 +24,6 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    params.re
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
